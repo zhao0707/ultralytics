5a626f2d30bc3d50e9d352817239a193fe0f8b29
Laughing-q <1185102784@qq.com>
2024-08-09 17:55:09 +0800

    Update loss.py

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -734,9 +734,16 @@ class E2EDetectLoss:
 
     def __call__(self, preds, batch):
         """Calculate the sum of the loss for box, cls and dfl multiplied by batch size."""
+        o2m = batch.get("o2m", None)
+        print(o2m)
+        if o2m is not None:
+            o2o = max(1.0 - o2m, 0)
+        else:
+            o2m = o2o = 1
+
         preds = preds[1] if isinstance(preds, tuple) else preds
         one2many = preds["one2many"]
         loss_one2many = self.one2many(one2many, batch)
         one2one = preds["one2one"]
         loss_one2one = self.one2one(one2one, batch)
-        return loss_one2many[0] + loss_one2one[0], loss_one2many[1] + loss_one2one[1]
+        return loss_one2many[0] * o2m + loss_one2one[0] * o2o, loss_one2many[1] * o2m + loss_one2one[1] * o2o
